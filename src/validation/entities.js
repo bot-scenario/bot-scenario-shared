f35b26f66df588a8716d31a318a1cb9e2f2a3cac
Haim Rubin <haim.rubin@gmail.com>
2023-03-18 14:18:58 +0200

    Read applications from constants

--- src/validation/entities.js
+++ src/validation/entities.js
@@ -1,4 +1,5 @@
 import joi from 'joi'
+import { APPLICATION } from '../services-util/constants/applications.js'
 
 export const configurationSchema = joi.object().keys({
   token: joi.string().required(),
@@ -8,5 +9,8 @@ export const configurationSchema = joi.object().keys({
     .pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
   _id: joi.string(),
   name: joi.string().required(),
-  application: joi.string().valid('whatsapp', 'telegram').required(),
+  application: joi
+    .string()
+    .valid(...Object.values(APPLICATION))
+    .required(),
 })
