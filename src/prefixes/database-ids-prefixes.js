2a8dc58b618bc3e42c43c9d96da126d029b5d609
Haim Rubin <haim.rubin@gmail.com>
2023-07-05 17:04:10 +0300

    Added prefix for scenario version

--- src/prefixes/database-ids-prefixes.js
+++ src/prefixes/database-ids-prefixes.js
@@ -1,6 +1,7 @@
-export const DATABASE_ID_PREFIX = {
+export const DATABASE_ID_PREFIX = Object.freeze({
   CONFIGURATION: 'conf',
   SCENARIO: 'scn',
+  SCENARIO_VERSION: 'scn_ver',
   NODE: 'node',
   FILE: 'file',
-}
+})
